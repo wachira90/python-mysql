b1422774081e333d7b76abcab805b7ff41f6c963
Wachira Duangdee <wcrcorp@gmail.com>
2021-07-05 20:28:33 +0700

    Create 3-connect2.py

---
+++ 3-connect2.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection_config_dict = {
+        'user': 'pynative',
+        'password': 'pynative@123',
+        'host': '127.0.0.1',
+        'database': 'Electronics',
+        'raise_on_warnings': True,
+        'use_pure': False,
+        'autocommit': True,
+        'pool_size': 5
+    }
+    connection = mysql.connector.connect(**connection_config_dict)
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Your connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
