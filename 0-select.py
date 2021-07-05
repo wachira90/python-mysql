a45375bc2dafb132b7730ed12c9f9db9c628e597
Wachira Duangdee <wcrcorp@gmail.com>
2021-07-05 20:31:40 +0700

    Create 0-select.py

---
+++ 0-select.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='electronics',
+                                         user='pynative',
+                                         password='pynative@#29')
+
+    sql_select_Query = "select * from Laptop"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+    print("Total number of rows in table: ", cursor.rowcount)
+
+    print("\nPrinting each row")
+    for row in records:
+        print("Id = ", row[0], )
+        print("Name = ", row[1])
+        print("Price  = ", row[2])
+        print("Purchase date  = ", row[3], "\n")
+
+except mysql.connector.Error as e:
+    print("Error reading data from MySQL table", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        cursor.close()
+        print("MySQL connection is closed")
